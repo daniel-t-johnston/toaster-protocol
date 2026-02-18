f8e3839bb8defb4f748ab522c16949deac569fa8
Danny <daniel.t.johnston@gmail.com>
2026-02-17 23:26:52 -0500

    Added all TODOs for Cipher

--- Cipher.Core/Cipher.cpp
+++ Cipher.Core/Cipher.cpp
@@ -2,7 +2,33 @@
 #include <string>
 #include "Cipher.h"
 
-std::string test()
+void generate_key(std::string keyword, char key[52][2])
 {
-	return "This is a test.";	
+	//TODO: Generate key a-z, A-Z in COL 0, shifted text in COL 1
 }
+
+std::string get_input( std::string input_file)
+{
+	std::string input_string;
+
+	//TODO: Write input_file to input_string 
+
+	return input_string;
+}
+
+std::string shift_text(std::string input_string, char key[52][2], char action)
+{
+	std::string output_string;
+
+	//TODO: Replace chars found in COL 0 with COL 1 to encrypt, reverse for decrypt
+
+	return output_string;
+}
+
+int save_output(std::string OUTPUT_FILE, std::string output_string)
+{
+	//TODO: Save output string to file
+
+	return 1;
+}
+
